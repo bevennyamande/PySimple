6d5a908934d5e11c72737b41f81c09eb6fdfdcf6
Tyler Fenby <tylerfenby@gmail.com>
2014-12-20 12:25:28 -0500

    Improve testing w/ tox
    setuptools testing is awful. Use tox for everything.
    
    - Remove unnecessary depends from tox.ini
    - Remove setup.py test integration
    - Coverage testing in tox
    - Improve tox flake8 config
    - Run tox with Travis

--- setup.py
+++ setup.py
@@ -1,30 +1,8 @@
 #!/usr/bin/env python
 
-import sys
-
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 from pysimple import version
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 readme = open('README.rst').read()
@@ -58,9 +36,9 @@ setup(
         'requests',
         'lxml',
     ],
-    tests_require=['pytest'],
+    tests_require=[],
     setup_requires=[],
-    cmdclass={'test': PyTest},
+    cmdclass={},
     keywords='pysimple simple',
     classifiers=[
         'Development Status :: 4 - Beta',
